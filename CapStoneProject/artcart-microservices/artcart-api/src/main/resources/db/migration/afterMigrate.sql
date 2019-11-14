59b57423b110b3971713639a0c1a56d2ec2e58f3
rosendopili <rosendopili@gmail.com>
2019-11-14 14:40:12 -0500

    ensuring I have the latest version

--- CapStoneProject/artcart-microservices/artcart-api/src/main/resources/db/migration/afterMigrate.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO users(username, email, password)
-VALUES ('Rosendo', 'test@email.com', 'access'),
-('Sheryl', 'Sheryl@Email.com', 'access'),
-('Jeff', 'jeff@email.com', 'access'),
-('John', 'john@email.com', 'access'),
-('Katie', 'Katie@email.com', 'access'),
-('Mark', 'Mark@email.com', 'access'),
-('Charlene', 'Charlene@email.com', 'access'),
-('Mary', 'Mary@email.com', 'access');
